654f1ca69168f18f1b089e126db8bce706ef10af
Anton064 <63185453+Anton064@users.noreply.github.com>
2020-09-13 13:49:43 +0300

    Add files via upload

---
+++ Generator.py
@@ -0,0 +1,19 @@
+#Генератор паролей. Выдаёт рандомный пароль.
+
+print("2811735")
+print("_" * 12)
+
+import random as lock
+password = ""
+for q in range (7): password += lock.choice(list("2811735"))
+print(password)
+print("" * 10)
+print("password generator")
+print("" * 10)
+print("210120083511")
+print("_" * 12)
+
+import random as lock
+password2 = ""
+for w in range (12): password2 += lock.choice(list("210120083511"))
+print(password2)
